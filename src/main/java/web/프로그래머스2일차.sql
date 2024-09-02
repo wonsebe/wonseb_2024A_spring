fa7466062e9deee7dbefc02ef235a35bbaf70acf
wonsebe <wonsb03129@gmail.com>
2024-09-02 16:35:54 +0900

    자리 바꾸기 전

--- src/main/java/web/프로그래머스2일차.sql
+++ src/main/java/web/프로그래머스2일차.sql
@@ -60,17 +60,45 @@
     ORDER BY TOTAL_ORDER;
 
     # 13.[GROUP BY]진료과별 총 예약 횟수 출력하기
-
+    SELECT MCDP_CD as 진료과코드 , count(APNT_NO) as 5월예약건수
+    from APPOINTMENT
+    where month(APNT_YMD) = '05' and year(APNT_YMD) = '2022'
+    group by MCDP_CD
+    order by count(MCDP_CD), MCDP_CD;
 
     # 14.[GROUP BY]고양이와 개는 몇 마리 있을까
+    SELECT ANIMAL_TYPE, count(ANIMAL_TYPE) as count
+    from ANIMAL_INS
+    group by ANIMAL_TYPE
+    order by ANIMAL_TYPE
 
     # 15.[GROUP BY]동명 동물 수 찾기
+    SELECT NAME, count(NAME) as COUNT
+    from ANIMAL_INS
+    where NAME is  not null
+    group by NAME
+    having count(NAME)>=2
+    order by name
 
-    # 16. [GROUP BY]조건에 맞는 사원 정보 조회하기
+    # 16. [GROUP BY]
+
+
 
     # 17. [GROUP BY]물고기 종류 별 잡은 수 구하기
+    SELECT COUNT(TIME) AS FISH_COUNT, FISH_NAME
+    FROM FISH_INFO
+    JOIN FISH_NAME_INFO
+    USING(FISH_TYPE)
+    GROUP BY FISH_NAME
+    ORDER BY FISH_COUNT DESC
 
     # 18. [JOIN]상품 별 오프라인 매출 구하기
+    SELECT P.PRODUCT_CODE, SUM(P.PRICE*O.SALES_AMOUNT) AS SALES
+    FROM PRODUCT AS P
+    JOIN OFFLINE_SALE  AS O
+    ON P.PRODUCT_ID = O.PRODUCT_ID
+    GROUP BY P.PRODUCT_CODE
+    ORDER BY SALES DESC, P.PRODUCT_CODE ASC
 
 
 
